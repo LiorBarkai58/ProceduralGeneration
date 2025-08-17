5cd543c5bb76df0671d5140d3a5c7561285b50da
Sagrius <alexeibuic@gmail.com>
2025-08-17 20:34:30 +0300

    Created definitions for rooms

---
+++ Assets/Scripts/GlobalEnums.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public enum RoomType {Type1,Type2,Type3 };
+public enum RoomShape { Shape1, Shape2, Shape3 };
+public enum PlacementRules { Rule1, Rule2, Rule3 };
+
+public enum DistanceRules { Rule1,Rule2,Rule3 };
+public class GlobalEnums : MonoBehaviour
+{
+    //Script for holding all the global enums at one place for easier managment
+}
