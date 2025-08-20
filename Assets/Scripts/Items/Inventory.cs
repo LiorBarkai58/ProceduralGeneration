609ae251b8425597c4f61462a088a23d5b783a24
Sagrius <alexeibuic@gmail.com>
2025-08-20 20:46:42 +0300

    making inventory and items

---
+++ Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<ItemBase> DisplayList = new();
+    private List <ItemBase> _itemInventory = new();
+
+    public void AddItem(ItemBase item)
+    {
+        _itemInventory.Add(item);
+        DisplayList = _itemInventory;
+    }
+    public void ConsumeItem(ItemBase item)
+    {
+        if(_itemInventory.Contains(item)) _itemInventory.Remove(item);
+        DisplayList = _itemInventory;
+
+    }
+
+    public bool DoesInventoryContain(ItemType type,int id,bool consumeItem = false)
+    {
+        ItemBase FoundItem = null;
+
+        foreach (var item in _itemInventory)
+        {
+            if (item.GetItemInfo().Item1 == type && item.GetItemInfo().Item2 == id)
+            {
+                if (consumeItem)
+                {
+                    FoundItem = item;
+                    break;
+                }
+                return true;
+            }
+        }
+
+        if (FoundItem)
+        {
+            ConsumeItem(FoundItem);
+            return true;
+        } 
+
+        return false;
+    }
+    public ItemBase GetItem(ItemType type, int id)
+    {
+        foreach (var item in _itemInventory)
+        {
+            if (item.GetItemInfo().Item1 == type && item.GetItemInfo().Item2 == id)
+            {
+                return item;
+            }
+
+        }
+
+        return null;
+    }
+   
+}
